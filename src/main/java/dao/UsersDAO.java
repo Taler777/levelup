ac6a38dae79a75d8d9bf310a78ac42b4377a6d3f
SypchenkoAI <sypchenko.a.i@sberbank.ru>
2019-11-03 22:56:22 +0300

    DAO layer for User with test

---
+++ src/main/java/dao/UsersDAO.java
@@ -0,0 +1,33 @@
+package dao;
+
+import entities.User;
+
+import javax.persistence.EntityManager;
+
+public class UsersDAO
+{
+	private EntityManager manager;
+
+	public UsersDAO(EntityManager manager)
+	{
+		this.manager = manager;
+	}
+
+	public void create(User user)
+	{
+		manager.persist(user);
+	}
+
+	public User findByLogin(String login){
+		return manager.createQuery("from User where login = :p", User.class)
+			.setParameter("p", login)
+			.getSingleResult();
+	}
+
+	public User findByLoginAndPassword(String login, String password){
+		return manager.createQuery("from User where login = :login AND password = :password", User.class)
+			.setParameter("login", login)
+			.setParameter("password",password)
+			.getSingleResult();
+	}
+}
---
+++ src/test/java/dao/UsersDAOTest.java
@@ -0,0 +1,100 @@
+package dao;
+
+import entities.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import static org.junit.Assert.*;
+
+public class UsersDAOTest
+{
+	private EntityManagerFactory factory;
+	private EntityManager manager;
+	private UsersDAO dao;
+	private User user;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		factory = Persistence.createEntityManagerFactory("TestPersistenceUnit");
+		manager = factory.createEntityManager();
+		dao = new UsersDAO(manager);
+		user = new User();
+		user.setLogin("test");
+		user.setPassword("123");
+	}
+
+	@After
+	public void cleanup()
+	{
+		if (manager != null)
+		{
+			manager.close();
+		}
+		if (factory != null)
+		{
+			factory.close();
+		}
+	}
+
+	@Test
+	public void create() throws Exception
+	{
+		manager.getTransaction().begin();
+		try
+		{
+			dao.create(user);
+			manager.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			manager.getTransaction().rollback();
+			throw e;
+		}
+		assertNotNull(manager.find(User.class, user.getId()));
+	}
+
+	@Test
+	public void findByLogin() throws Exception
+	{
+		manager.getTransaction().begin();
+		try
+		{
+			manager.persist(user);
+			manager.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			manager.getTransaction().rollback();
+			throw e;
+		}
+		User found = dao.findByLogin("test");
+		assertNotNull(found);
+		assertEquals(user.getId(), found.getId());
+	}
+
+	@Test
+	public void findByLoginAndPassword() throws Exception
+	{
+		manager.getTransaction().begin();
+		try
+		{
+			manager.persist(user);
+			manager.getTransaction().commit();
+		}
+		catch (Exception e)
+		{
+			manager.getTransaction().rollback();
+			throw e;
+		}
+
+		User found = dao.findByLoginAndPassword("test", "123");
+		assertNotNull(found);
+		assertEquals(user.getId(), found.getId());
+	}
+}
